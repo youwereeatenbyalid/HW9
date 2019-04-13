112e941996a700ac66e6f1684fb8e9532af49d2d
youwereeatenbyalid <youwereeatenbyalid@github.com>
2019-04-13 15:09:02 -0400

    Big commit?

---
+++ build/CMakeFiles/hw9.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/hw9.dir/main.cpp.o"
+  "hw9.pdb"
+  "hw9"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/hw9.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
--- main.cpp
+++ main.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 
 int main(int argc, char **argv) {
-    std::cout << "Made an edit!" << std::endl;
+    std::cout << "New edit!" << std::endl;
     return 0;
 }
